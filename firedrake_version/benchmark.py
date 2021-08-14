f90f5c9315a53cb94d7d277be97504afdcd65653
akhil <akhil2@illinois.edu>
2021-08-14 13:03:17 +0530

    aesthetic changes to benchmark with better command-line arguments

--- firedrake_version/benchmark.py
+++ firedrake_version/benchmark.py
@@ -1,39 +1,38 @@
 from firedrake import *
 from tri_mqc import TriangleMeshQualityCalculator
 from cqm_parloop import getCQM
+from argparse import ArgumentParser
 import time
 import sys
 
 def main():
-    print ("Benchmark testing for par_loop vs Python implementation of CQM calculation.")
-    num_trials = 50         # I guess I should make this configurable too
-    if len(sys.argv) < 3:
-        print ("Defaulting to a benchmark of 50 x 50 cells, with {} trials".format(num_trials))
-        m, n = 50, 50
-    elif len(sys.argv) == 3:
-        m, n = [int(k) for k in sys.argv[1:]]
-    elif len(sys.argv) == 4:
-        m, n, num_trials = [int(k) for k in sys.argv[1:]]
-    else:
-        raise ValueError("Illegal number of arguments")
-
-    # print ("m, n: {}, {}".format(m, n))
+    parser = ArgumentParser(description=\
+        'Enter benchmark inputs (m, n, N) to test calculation of mesh quality measures using \
+        native Python (with numpy) vs using a C-kernel and Firedrake par_loops. Meshes are \
+        2D triangular meshes on the Unit Square with identical elements. \
+        Values of CQM should be equal for all elements.')
+    parser.add_argument('-m', default=50, type=int, help='Number of cells in the x-direction')
+    parser.add_argument('-n', default=50, type=int, help='Number of cells in the y-direction')
+    parser.add_argument('-N', default=20, type=int, help='Number of trials for benchmark')
+    args = parser.parse_args()
+    m, n, num_trials = args.m, args.n, args.N
+    
     mesh = UnitSquareMesh(m, n)
     tmqc = TriangleMeshQualityCalculator(mesh)
     cStart, cEnd = tmqc.getCellIndices()
-    print ("Mesh size: {} x {}".format(m, n))
+    print ("Mesh size (m x n): {} x {}".format(m, n))
     print ("Number of cells: {}".format(cEnd - cStart))
     print ("Number of trials: {}".format(num_trials))
     
     parLoopTimes = 0
     nativePyTimes = 0
     for t in range(num_trials):
-        if t % (num_trials//20) == 0:
+        if t % (num_trials // min(20, num_trials)) == 0:
             print ('.', end='')
             sys.stdout.flush()
         
         start = time.time()
-        _, _, _, _, _, _ = getCQM(mesh)
+        _ = getCQM(mesh)
         parLoopTimes += time.time() - start
         
         start = time.time()
@@ -46,6 +45,7 @@ def main():
     
     print ("\nAverage time with par_loop: {}s".format(averageParLoopTime))
     print ("Average time with native Python: {}s".format(averageNativePyTime))
+    print ("Speedup: {:.1f}x".format(averageNativePyTime / averageParLoopTime))
 
         
 
