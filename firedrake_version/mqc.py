914d0f9cb14b0e6dc4efe9bc54a2b946b6f7d64c
akhil <akhil2@illinois.edu>
2021-09-07 17:15:54 +0530

    Fix segfault issue

--- firedrake_version/mqc.py
+++ firedrake_version/mqc.py
@@ -26,7 +26,6 @@ class MeshQualityCalculator(object):
         self.mesh = mesh
         self.dim = mesh._geometric_dimension
         self.plex = mesh._topology.topology_dm
-        self.plex.distributeOverlap(overlap=1)
         self.__makeCoordinateMap()          # sets self.sec
         self.coordArray = self.plex.getCoordinates().array
         print ("coordArray shape: {}".format(self.coordArray.shape))
